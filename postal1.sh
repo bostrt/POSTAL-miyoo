83d4c498fa2dcff6f69060193b40906e25357506
Robert Bost <rbost@redhat.com>
2024-07-06 16:06:51 -0600

    keys: Update keys for Miyoo Mini+ and use POSTAL.INI as main config instead of making a home dir

--- postal1.sh
+++ postal1.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-EGL_VIDEODRIVER=mmiyoo SDL_VIDEODRIVER=mmiyoo SDL_AUDIODRIVER=mmiyoo LD_LIBRARY_PATH=/mnt/SDCARD/Roms/PORTS/Games/Postal/lib:$LD_LIBRARY_PATH /mnt/SDCARD/Roms/PORTS/Games/Postal/postal1-arm
+EGL_VIDEODRIVER=mmiyoo SDL_VIDEODRIVER=mmiyoo SDL_AUDIODRIVER=mmiyoo LD_LIBRARY_PATH=/mnt/SDCARD/Roms/PORTS/Games/Postal/lib:$LD_LIBRARY_PATH /mnt/SDCARD/Roms/PORTS/Games/Postal/postal1-arm --nohomedir
